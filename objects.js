6551b385ade357c2a0ea026064d2c4f9586e4620
Paul Baluyot <zero4732@yahoo.com>
2018-11-26 21:14:59 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -1,4 +1,4 @@
 var playlist = { artist: "song" };
 function updatePlaylist(playlist, artistName, songTitle) {
-  var playlist = new Object(playlist, artistName, songTitle);
+  playlist[artistName]
 }
