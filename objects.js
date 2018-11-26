0d03cd3ed8c346011e41f6739cf4942b87386723
Paul Baluyot <zero4732@yahoo.com>
2018-11-26 21:17:26 +0000

    automatically backed up by learn

--- objects.js
+++ objects.js
@@ -3,5 +3,5 @@ function updatePlaylist(playlist, artistName, songTitle) {
   playlist[artistName]=songTitle;
 }
 function removeFromPlaylist(playlist, artistName) {
-  
+  delete playlist[artistName];
 }
